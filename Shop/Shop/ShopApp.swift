87336c07aeb94980a9ed9a249f958e219481e1b2
Shisetsu <shisetsu@icloud.com>
2022-01-04 12:11:49 +0300

    Add .gitignore for Swift

---
+++ Shop/Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Илья Дунаев on 04.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
